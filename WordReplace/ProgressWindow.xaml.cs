908576461366d386a127ddc265098cc8b07dfbf3
Konstantin Alekseev <kv.alekseev@gmail.com>
2015-02-01 01:10:00 +0300

    Add forgotten files

---
+++ WordReplace/ProgressWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+
+namespace WordReplace
+{
+    public partial class ProgressWindow : Window
+    {
+        private int Counter = 0;
+        private static object syncCounter = new object();
+
+        public ProgressWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Update(object sender, ProgressChangedEventArgs e) {
+            lock(syncCounter)
+                Counter++;
+            pbStatus.Value = 100 * Counter / (double)e.ProgressPercentage;
+        }
+
+        public void Reset()
+        {
+            Counter = 0;
+            pbStatus.Value = 0;
+        }
+    }
+}
